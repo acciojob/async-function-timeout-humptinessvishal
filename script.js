80e2976d338cbaa9f55de5e194d5d7184b99c621
Vishal Kumar <136982736+humptinessvishal@users.noreply.github.com>
2023-08-29 10:17:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const button = document.getElementById('btn');
+button.addEventListener('click', displayDelayedMessage);
+async function displayDelayedMessage() {
+	const textInput = document.getElementById('text');
+    const delayInput = document.getElementById('delay');
+    const outputDiv = document.getElementById('output');
+    const message = textInput.value;
+    const delay = delayInput.value;
+	if (!message || delay <= 0) {
+		outputDiv.textContent = 'Please enter a valid message and delay.';
+		return;
+	}
+	await new Promise(resolve => setTimeout(resolve, delay));
+	outputDiv.textContent = message;
+}
